c3b92d54cd06d61c5305a92b6ede392d6c0300fd
Dragon Wolf Leo <dleowolf@gmail.com>
2019-11-01 01:41:34 -0500

    Change bigportrait to the oval one

--- modmain.lua
+++ modmain.lua
@@ -33,10 +33,6 @@ Assets = {
 	
 	Asset( "IMAGE", "images/names_gold_esctemplate.tex" ),
     Asset( "ATLAS", "images/names_gold_esctemplate.xml" ),
-	
-    Asset( "IMAGE", "bigportraits/esctemplate_none.tex" ),
-    Asset( "ATLAS", "bigportraits/esctemplate_none.xml" ),
-
 }
 
 AddMinimapAtlas("images/map_icons/esctemplate.xml")
@@ -58,9 +54,3 @@ STRINGS.NAMES.ESCTEMPLATE = "Esc"
 
 -- Add mod character to mod character list. Also specify a gender. Possible genders are MALE, FEMALE, ROBOT, NEUTRAL, and PLURAL.
 AddModCharacter("esctemplate", "FEMALE")
-
---dumb fix because Klei is dumb and changed the way bigportraits work without adjusting the API
---I hope this will be redundant in an update, but considering how long it's been since the issue came up, I doubt it. -M
--- Set this to use the correct (oval) bigportrait in the new character select screen. The old (square) bigportrait is still used for Inspect.
-GLOBAL.PREFAB_SKINS["esctemplate"] = {"esctemplate_none"}
-GLOBAL.PREFAB_SKINS_IDS["esctemplate"] = {["esctemplate_none"] = 1}
